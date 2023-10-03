3b37acafc50308a1d44c8fd541684f1114cad937
566325 <nethisireesha288@gmail.com>
2023-10-03 11:15:50 +0530

    [Sireesha]:Added Annotations Examples

---
+++ Annotations/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Annotations
+{
+    internal class Employee
+    {
+        [Required(ErrorMessage ="Employee Name is Required")]
+        [StringLength(100,MinimumLength =3,ErrorMessage ="Name should have Minimum 3 characters and max 100")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [Range(18,99,ErrorMessage ="Age should be 18 and 99")]
+        public int Age { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
---
+++ Annotations/ValidationClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Annotations
+{
+    public class ValidationClass
+    {
+        public static void Test()
+        {
+            Employee employee = new Employee();
+            employee.Name = "Sireesha";
+            employee.Age = 19;
+            employee.Phone = " 63764899754";
+            employee.Email = "Siri@gmail.com";
+
+            ValidationContext contect = new ValidationContext(employee, null, null);
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+            bool vaild =Validator.TryValidateObject(employee, contect,validationResults,true);
+
+            if (!vaild)
+            {
+                foreach(ValidationResult validationResult in validationResults)
+                {
+                    Console.WriteLine(validationResult);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Name :"+ employee.Name +" Age:" + employee.Age +" Phone: " + employee.Phone +  " Email Address: "+ employee.Email);
+            }
+
+
+        }
+    }
+}
